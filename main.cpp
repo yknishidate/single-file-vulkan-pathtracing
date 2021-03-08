c11047e3fe22e2654883069ed52ced5ea503b21c
nishidate-yuki <asta18425@gmail.com>
2021-03-09 01:30:51 +0900

    create instance

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 
 #include <iostream>
 
-#include <GLFW/glfw3.h>
-
 #define VULKAN_HPP_DISPATCH_LOADER_DYNAMIC 1
 #include <vulkan/vulkan.hpp>
+VULKAN_HPP_DEFAULT_DISPATCH_LOADER_DYNAMIC_STORAGE
+
+#include <GLFW/glfw3.h> // include after vulkan.hpp
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
@@ -43,15 +44,146 @@ const bool enableValidationLayers = true;
 #else
 const bool enableValidationLayers = false;
 #endif
+std::vector<const char*> validationLayers;
+
+// Debug callback
+VKAPI_ATTR VkBool32 VKAPI_CALL
+debugUtilsMessengerCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+                            VkDebugUtilsMessageTypeFlagsEXT messageTypes,
+                            VkDebugUtilsMessengerCallbackDataEXT const* pCallbackData,
+                            void* /*pUserData*/)
+{
+    std::cerr << "messageIndexName   = " << pCallbackData->pMessageIdName << "\n";
+    for (uint8_t i = 0; i < pCallbackData->objectCount; i++) {
+        std::cerr << "objectType      = " << vk::to_string(static_cast<vk::ObjectType>(
+            pCallbackData->pObjects[i].objectType)) << "\n";
+    }
+    std::cerr << pCallbackData->pMessage << "\n\n";
+    return VK_FALSE;
+}
+
+class Application
+{
+public:
+
+    ~Application()
+    {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+
+    void run()
+    {
+        initWindow();
+        initVulkan();
+        mainLoop();
+    }
+
+private:
+
+    GLFWwindow* window;
+    vk::UniqueInstance instance;
+    std::vector<vk::PhysicalDevice> physicalDevices;
+    vk::UniqueDebugUtilsMessengerEXT messenger;
+    vk::UniqueSurfaceKHR surface;
+
+    void initWindow()
+    {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        window = glfwCreateWindow(WIDTH, HEIGHT, "VulkanPathtracing", nullptr, nullptr);
+    }
+
+    void initVulkan()
+    {
+        createInstance();
+        createSurface();
+        //device = std::make_unique<vkr::Device>(*instance, *surface);
+        //swapChain = std::make_unique<vkr::SwapChain>(*device, vk::Extent2D{ WIDTH, HEIGHT });
+
+        //storageImage = swapChain->createStorageImage();
+
+        //buildAccelStruct();
+
+        //loadShaders();
+
+        //createDescSets();
+
+        //pipeline = descSets->createRayTracingPipeline(*shaderManager, 1);
+        //shaderManager->initShaderBindingTable(*pipeline, 1, 1, 1);
+        //swapChain->initDrawCommandBuffers(*pipeline, *descSets, *shaderManager, *storageImage);
+    }
+
+    void mainLoop()
+    {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
+
+    void createInstance()
+    {
+        // Get GLFW extensions
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+        // Create DynamicLoader (see https://github.com/KhronosGroup/Vulkan-Hpp)
+        static vk::DynamicLoader dl;
+        auto vkGetInstanceProcAddr = dl.getProcAddress<PFN_vkGetInstanceProcAddr>("vkGetInstanceProcAddr");
+        VULKAN_HPP_DEFAULT_DISPATCHER.init(vkGetInstanceProcAddr);
+
+        // Add validation layer, extension
+        if (enableValidationLayers) {
+            validationLayers.push_back("VK_LAYER_KHRONOS_validation");
+            extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+        }
+
+        // Create instance
+        vk::ApplicationInfo appInfo;
+        appInfo.setPApplicationName("VulkanPathtracing");
+        appInfo.setApiVersion(VK_API_VERSION_1_2);
+        instance = vk::createInstanceUnique({ {}, &appInfo, validationLayers, extensions });
+        VULKAN_HPP_DEFAULT_DISPATCHER.init(*instance);
+
+        // Get physical devices
+        physicalDevices = instance->enumeratePhysicalDevices();
+
+        if (enableValidationLayers) {
+            createDebugMessenger();
+        }
+    }
+
+    void createDebugMessenger()
+    {
+        vk::DebugUtilsMessageSeverityFlagsEXT severityFlags{
+            vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning
+            | vk::DebugUtilsMessageSeverityFlagBitsEXT::eError };
+
+        vk::DebugUtilsMessageTypeFlagsEXT messageTypeFlags{
+            vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral
+            | vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance
+            | vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation };
+
+        messenger = instance->createDebugUtilsMessengerEXTUnique(
+            { {}, severityFlags, messageTypeFlags, &debugUtilsMessengerCallback });
+    }
+
+    void createSurface()
+    {
+        VkSurfaceKHR _surface;
+        if (glfwCreateWindowSurface(VkInstance(*instance), window, nullptr, &_surface) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create window surface!");
+        }
+        vk::ObjectDestroy<vk::Instance, VULKAN_HPP_DEFAULT_DISPATCHER_TYPE> _deleter(*instance);
+        surface = vk::UniqueSurfaceKHR(vk::SurfaceKHR(_surface), _deleter);
+    }
+};
 
 int main()
 {
-    glfwInit();
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-    auto window = glfwCreateWindow(WIDTH, HEIGHT, "VulkanPathtracing", nullptr, nullptr);
-
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-    }
+    Application app;
+    app.run();
 }
