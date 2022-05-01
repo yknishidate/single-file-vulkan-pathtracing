9619f796898171cf0d22cf5c9610c8efbdf61704
Yuki Nishidate <asta18425@gmail.com>
2022-05-01 14:34:51 +0900

    refactor

---
+++ shaders/compile.bat
@@ -0,0 +1,3 @@
+%VULKAN_SDK%/Bin/glslc.exe raygen.rgen -o raygen.rgen.spv --target-env=vulkan1.2
+%VULKAN_SDK%/Bin/glslc.exe closesthit.rchit -o closesthit.rchit.spv --target-env=vulkan1.2
+%VULKAN_SDK%/Bin/glslc.exe miss.rmiss -o miss.rmiss.spv --target-env=vulkan1.2
